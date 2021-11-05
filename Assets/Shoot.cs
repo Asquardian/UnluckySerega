2cd37ac0e9683a136b7744fa44130da5fcf430af
dainl <alexromanov5262@gmail.com>
2021-11-05 21:58:31 +0300

    Begin 4 lvl

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -2,25 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SafeToKillable : MonoBehaviour
+public class Shoot : MonoBehaviour
 {
+    public Rigidbody2D item;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Safe")
+        if (collision.gameObject.tag == "Player")
         {
-            Destroy(collision.gameObject, 0);
-        }
-        if (collision.gameObject.tag == "Killable")
-        {
-            Destroy(collision.gameObject, 0);
+            Debug.Log("shoot");
+            item.velocity = new Vector3(-30f, 0, 0);
+            Destroy(gameObject, 0f);
         }
     }
-
     // Update is called once per frame
     void Update()
     {
