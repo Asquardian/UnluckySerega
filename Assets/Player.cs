d8ea7ad8318d88b7235db0ae3a3ea9aefcb87f3f
kontra <kontr@DESKTOP-IBGVV8O>
2021-11-05 16:34:45 +0700

    Add animations

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,15 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
     private float speed = 10.0f;
+    [SerializeField]
     private float JumpForce = 25.0f;
     private int JumpCount;
     private bool IsJumped;
     private Rigidbody2D rb;
     public GameObject Sprite;
+    private Animator anim;
+    private int move;
+    private bool isFacingRight = true;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         JumpCount = 0;
     }
@@ -29,13 +35,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
         if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+           
+            anim.SetFloat("Speed", Mathf.Abs(move));
         }
         if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
+       
+            anim.SetFloat("Speed", Mathf.Abs(move));
         }
         if ((Input.GetKeyDown("w") || Input.GetKeyDown("space") || Input.GetKeyDown(KeyCode.UpArrow)) && JumpCount < 2 && IsJumped) //For Double Jump
         {
@@ -46,11 +58,29 @@ public class Player : MonoBehaviour
         {
             IsJumped = false;
         }
+
+        if (move > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if(move < 0 && isFacingRight)
+        {
+            Flip();
+        }
     }
+    
 
     public void death()
     {
         Destroy(Sprite, 0f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    
     }
 }
